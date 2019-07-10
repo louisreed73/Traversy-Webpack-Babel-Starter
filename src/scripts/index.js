695c3d306be5354bc76eb31893ce596a71845e4e
louisreed73 <luis.sa1973@gmail.com>
2019-07-10 20:46:23 +0200

    inicio

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+
+// import '../styles/global_variables';
+import '../styles/index.scss';
+
+import {saludo} from  './modulo.js';
+console.log('webpack starterkit');
+
+saludo("Hijos de puta!!!!!!");
---
+++ src/scripts/modulo.js
@@ -0,0 +1,5 @@
+
+
+export let saludo=(como)=>{
+    console.log(`Desde el modulo 0, hago esto: ${como}`);
+};
